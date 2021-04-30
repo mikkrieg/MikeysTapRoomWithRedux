aa4713e603506bbae2965ebb206e23b99f367e9b
Mikey Kriegel <mikkrieg@gmail.com>
2021-04-30 10:09:12 -0700

    Add semicolons, add action constructor

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -8,5 +8,5 @@ describe("formVisibleReducer", () => {
 
   test('Should toggle form visibility state to true', () => {
     expect(formVisibleReducer(false, {type: c.TOGGLE_FORM})).toEqual(true);
-  })
-})
+  });
+});
