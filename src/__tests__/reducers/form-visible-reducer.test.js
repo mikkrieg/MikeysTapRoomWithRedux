4ab5caa020282172623f03698260fec5b4c27f42
Mikey Kriegel <mikkrieg@gmail.com>
2021-04-30 09:46:03 -0700

    Ran into testing bug, resolved, pass first reducer test

---
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -0,0 +1,10 @@
+import formVisibleReducer from './../../reducers/form-visible-reducer';
+
+
+describe("formVisibleReducer", () => {
+  test('Should return default state if no action is recognized', () => {
+    expect(formVisibleReducer(false, {type: null})).toEqual(false);
+  });
+
+
+})
