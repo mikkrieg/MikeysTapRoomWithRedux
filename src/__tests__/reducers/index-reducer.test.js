3ecc079f4bc2d7fd7df152c3790e016cb3b82028
Mikey Kriegel <mikkrieg@gmail.com>
2021-04-30 14:21:58 -0700

    Add two tests for root reducer, pass both

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,6 +1,12 @@
 import rootReducer from '../../reducers/index';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import masterMenuReducer from '../../reducers/master-menu-reducer';
+import * as c from '../../actions/ActionTypes';
+import { createStore } from 'redux';
+
 
 describe("rootReducer", () => {
+  let store = createStore(rootReducer);
 
   test('Should return default state if no action type is passed in', () => {
     expect(rootReducer({}, { type: null })).toEqual({
@@ -9,4 +15,28 @@ describe("rootReducer", () => {
     });
   });
 
+  test('Check that initial state of masterMenuReducer matches whats in root reducer', () => {
+    expect(store.getState().masterMenu).toEqual(masterMenuReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches whats in root reducer', () => {
+      expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that ADD_KEG action works for masterMenuReducer and root reducer', () => {
+    const action = {
+      type: c.ADD_KEG,
+      name: 'Coors Light',
+      brand: 'Coors',
+      price: 5,
+      alcohol: 4,
+      country: 'USA',
+      quantity: 20,
+      fullKeg: 124,
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterMenu).toEqual(masterMenuReducer(undefined, action));
+  });
+  
 });
